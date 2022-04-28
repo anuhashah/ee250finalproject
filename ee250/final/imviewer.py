f855abac7280587d5a2b92201024ea59040f4de2
Anuja Shah <apshah@usc.edu>
2022-04-27 23:55:38 -0700

    color name

--- ee250/final/imviewer.py
+++ ee250/final/imviewer.py
@@ -1,6 +1,3 @@
-from skimage.viewer import ImageViewer
-from skimage.io import imread
-
-img = imread('image.jpg') #path to IMG
-view = ImageViewer(img)
-view.show()
+from PIL import Image
+with Image.open('image.jpg') as img:
+    img.show()
